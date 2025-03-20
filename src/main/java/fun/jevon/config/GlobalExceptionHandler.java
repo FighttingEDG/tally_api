5a1755a185bea356900319408975998ce597775e
sjw <https://github.com/FighttingEDG/ag-end.git>
2025-03-20 15:51:34 +0800

    定义返回类，全局异常处理

--- src/main/java/fun/jevon/config/GlobalExceptionHandler.java
+++ src/main/java/fun/jevon/config/GlobalExceptionHandler.java
@@ -1,7 +1,13 @@
 package fun.jevon.config;
 
+import fun.jevon.util.response.Response;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public Response<String> handleException(Exception e) {
+        return Response.error(500, "服务器内容错误：" + e.getMessage());
+    }
 }
---
+++ src/main/java/fun/jevon/util/response/Response.java
@@ -0,0 +1,32 @@
+package fun.jevon.util.response;
+
+// Api返回类,<T>任意数据类型
+public class Response<T> {
+    // 状态码
+    private int code;
+    // 消息
+    private String message;
+    // 数据<T>任意类型
+    private T data;
+
+    // 无参构造方法
+    public Response(){}
+
+    // 全参构造方法
+    public Response(int code,String message,T data){
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    // 声明方法，第一个<T>声明方法是泛类型方法
+    // 默认返回200及成功信息+泛类型数据
+    public static <T> Response<T> succes(T data) {
+        return new Response<>(200, "成功", data);
+    }
+
+    // 错误返回
+    public static <T> Response<T> error(int code, String message) {
+        return new Response<>(code, message, null);
+    }
+}
